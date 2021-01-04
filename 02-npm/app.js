3325160ba9111c00fec3198be0524e0af0fd39c2
Yahaya Hyellamoda Yohanna <yasholma1@gmail.com>
2021-01-04 13:27:05 +0100

    learn to publish npm package

--- 02-npm/app.js
+++ 02-npm/app.js
@@ -1,4 +1,5 @@
 const _ = require("underscore");
+const lion = require("@yasholma/lion-lib-1.0");
 
 const dummyData = [1, 2, 3, 4, 5];
 
@@ -6,3 +7,6 @@ const data = _.map(dummyData, (num) => num * num);
 const res = _.contains(dummyData, 3);
 console.log({ data });
 console.log({ res });
+
+const sum = lion.add(2, 3);
+console.log({ sum });
