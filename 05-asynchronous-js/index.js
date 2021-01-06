9c199b28e9e3778ba1890b7f5c0b7d39687382e8
Yahaya Hyellamoda Yohanna <yasholma1@gmail.com>
2021-01-06 11:16:32 +0100

    learn asynchronous javascript, creating promises and consuming promises, async and await

--- 05-asynchronous-js/index.js
+++ 05-asynchronous-js/index.js
@@ -1,5 +1,31 @@
 console.log("Before");
-setTimeout(() => {
-  console.log("Reading a user from a database...");
-}, 2000);
+getUser(1)
+  .then((user) => getRepositories(user.githubUsername))
+  .then((repos) => console.log(repos))
+  .catch((err) => console.log(err.message));
+
 console.log("After");
+
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Reading a user from a database...");
+      resolve({
+        id,
+        githubUsername: "yasholma",
+      });
+    }, 2000);
+  });
+}
+
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    console.log("Fetching data...");
+    setTimeout(() => {
+      resolve({
+        username,
+        repos: ["repo1", "repo2", "repo3"],
+      });
+    }, 2000);
+  });
+}
